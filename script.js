0cc549059f2dd028c18761e596c7b6f3ab9a582e
Juebele <eronuebele@gmail.com>
2023-04-17 19:54:05 -0400

    Formatted HTML and started JS

---
+++ script.js
@@ -0,0 +1,10 @@
+
+var APIKey = "THQL4CERPJNZS1CR"
+// receive ticker input from user
+var ticker = ""
+// receive currency type from user
+var currencyType =""
+// add URL for ticker search API
+var tickerURL = ""; 
+// add URL for forex API
+var forexURL = ""
